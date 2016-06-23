8be928b26e8f673f17c42063484b91ad178267d3
Rick Ottten <rick@clarivoy.com>
2016-06-23 14:45:33 -0400

    convert to 3.0.3

--- src/main/java/com/maxdemarzi/pagerank/NodeCounter.java
+++ src/main/java/com/maxdemarzi/pagerank/NodeCounter.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class NodeCounter {
     public int getNodeCount(GraphDatabaseService db) {
-        Result result = db.execute( "CYPHER runtime=compiled MATCH (n) RETURN max(id(n)) AS maxId" );
+        Result result = db.execute( "CYPHER MATCH (n) RETURN max(id(n)) AS maxId" );
          Map response = result.next();
          if (response.get("maxId") != null) {
              return ((Number) response.get("maxId")).intValue() + 1;
@@ -16,7 +16,7 @@ public class NodeCounter {
          }
     }
     public int getRelationshipCount(GraphDatabaseService db) {
-        Result result = db.execute( "CYPHER runtime=compiled MATCH ()-[r]->() RETURN max(id(r)) AS maxId" );
+        Result result = db.execute( "CYPHER MATCH ()-[r]->() RETURN max(id(r)) AS maxId" );
          Map response = result.next();
          if (response.get("maxId") != null) {
              return ((Number) response.get("maxId")).intValue() + 1;
